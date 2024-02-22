b891d646c0ee5427f2dfa6fae2eee54afebdcdb7
Soporte Tecnico LitigioVirtual.com <litigiovirtualtech@hotmail.com>
2024-02-21 21:32:02 -0500

    Se realiza actividad

--- Program.cs
+++ Program.cs
@@ -2,11 +2,79 @@
 
 namespace MiPrimerHolaMundo
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numero;
+            string ingreso;
+            Console.Title = "Ejercicio Matematico en consola";
+
+            Console.WriteLine("Hola!");
+            Console.WriteLine();
+            System.Threading.Thread.Sleep(2000);
+            Console.WriteLine("Ingrese por favor 3 Numeros a los cuales se les hara las siguientes operaciones");
+            Console.WriteLine();
+            System.Threading.Thread.Sleep(3000);
+            Console.WriteLine("Primer numero -- Hallará el cuadrado");
+            Console.WriteLine("Segundo numero -- Hallará la raíz cuadrada");
+            Console.WriteLine("Tercer numero -- Hallará la raíz cúbica");
+            Console.WriteLine();
+            System.Threading.Thread.Sleep(5000);
+            Console.WriteLine("Si tiene todo claro empecemos!!");
+            Console.WriteLine();
+            System.Threading.Thread.Sleep(3000);
+            Console.WriteLine("Ingrese el Primer Numero y oprima  Enter");
+            Console.WriteLine();
+            ingreso = Console.ReadLine();
+            Console.WriteLine();
+            if (int.TryParse(ingreso, out numero))
+            {
+                var cuadradoResult = numero * numero;
+                Console.WriteLine("El " + numero + " elevado al cuadrado es " + cuadradoResult);
+            }
+            else
+            {
+                Console.WriteLine("El valor ingresado no es numerico");
+                Console.WriteLine();
+                Console.WriteLine("Chao");
+                return;
+            }
+            Console.WriteLine();
+            Console.WriteLine("Ingrese el Segundo Numero y oprima  Enter");
+            Console.WriteLine();
+            ingreso = Console.ReadLine();
+            if (int.TryParse(ingreso, out numero))
+            {
+                var raizCuadradaResult = Math.Sqrt(numero);
+                Console.WriteLine("La raiz cuadrada de  " + numero + " es  " + raizCuadradaResult);
+            }
+            else
+            {
+                Console.WriteLine("El valor ingresado no es numerico");
+                Console.WriteLine();
+                Console.WriteLine("Chao");
+                return;
+            }
+            Console.WriteLine();
+            Console.WriteLine("Ingrese el Tercer Numero y oprima  Enter");
+            Console.WriteLine();
+            ingreso = Console.ReadLine();
+            if (int.TryParse(ingreso, out numero))
+            {
+                var raizCubicaResult = Math.Pow(numero, 1.0 / 3.0);
+                Console.WriteLine("La raiz cubica de  " + numero + " es  " + raizCubicaResult);
+            }
+            else
+            {
+                Console.WriteLine("El valor ingresado no es numerico");
+                Console.WriteLine();
+                Console.WriteLine("Chao");
+                return;
+            }
+            Console.WriteLine();
+            System.Threading.Thread.Sleep(3000);
+            Console.WriteLine("Gracias");
         }
     }
 }
